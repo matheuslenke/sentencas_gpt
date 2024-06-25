763caef37aa4bde34c27377388290cc14042a639
Matheus Lenke <matheuslenke@gmail.com>
2024-06-25 14:58:27 -0300

    fix: some fixes

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 import argparse
 import asyncio
+import pandas as pd
+import matplotlib.pyplot as plt
 
 from src.extract_data import extract_data
 from src.json_to_csv import load_all_jsons
 from src.run_llms import change_category, run_llms
 from src.calculate_date import calculate_dates, date_to_csv
 from src.extract_sentences_time import extract_sentence_times
-from src.estimar_bayesiano import estimar_bayesiano
+
+folder_path = './dados_sentencas.xlsx'  # Replace with the actual path to the pena_dias folder
+
+def get_data():
+    df = pd.read_excel(folder_path, sheet_name=None)
+    combined_df = pd.concat(df.values(), ignore_index=True)
+    return combined_df
+
+def clean_data(df: pd.DataFrame):
+    df = df[df['tempo_preso'] != 0]
+    return df
+
+def estimar_bayesiano():
+    combined_df = get_data()
+    combined_df.describe()
+    combined_df = clean_data(combined_df)
+
+    unique_crimes = combined_df['tipo_crime'].unique()
+    fig, ax = plt.subplots(len(unique_crimes), 1, figsize=(10, 8))
+
+    for i, crime in enumerate(unique_crimes):
+        ax[i].hist(combined_df[combined_df['tipo_crime'] == crime]['tempo_preso'], bins=10)
+        ax[i].set_xlabel('Pena em Dias')
+        ax[i].set_ylabel('Frequência')
+        ax[i].set_title(f'Histograma da Distribuição de Pena em Dias para o Crime: {crime}')
+
+    plt.tight_layout()
+    plt.show()
+    fig, ax = plt.subplots(figsize=(10, 8))
+    for i, crime in enumerate(unique_crimes):
+        ax.hist(combined_df[combined_df['tipo_crime'] == crime]['tempo_preso'], bins=10, alpha=0.5, label=crime)
+    ax.set_xlabel('Pena em Dias')
+    ax.set_ylabel('Frequência')
+    ax.set_title('Histograma da Distribuição de Pena em Dias para Diferentes Crimes')
+    ax.legend()
+    plt.tight_layout()
+    plt.show()
 
 
 def main():
