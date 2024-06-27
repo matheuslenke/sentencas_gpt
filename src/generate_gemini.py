c30889dab9284a8926e86926b9f113c425432088
Matheus Lenke <matheuslenke@gmail.com>
2024-06-27 13:21:33 -0300

    fix gemini prompt

--- src/generate_gemini.py
+++ src/generate_gemini.py
@@ -1,12 +1,13 @@
+import time
 import vertexai
 from vertexai.generative_models import GenerativeModel
 import vertexai.preview.generative_models as generative_models
 
-from src.prompt import get_instructions_prompt
+from src.prompt import get_instructions_prompt, get_instructions_prompt_intermediate_sentence
 
 generation_config = {
     "max_output_tokens": 8192,
-    "temperature": 1,
+    "temperature": 0.7,
     "top_p": 0.95,
 }
 
@@ -17,7 +18,7 @@ safety_settings = {
     generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
 }
 
-def generate(message, prompt=get_instructions_prompt()):
+def generate(message, prompt=get_instructions_prompt_intermediate_sentence()):
     '''
         This function generates content using the Gemini model.
         message: The message from user input
@@ -25,7 +26,7 @@ def generate(message, prompt=get_instructions_prompt()):
     '''
     vertexai.init(project="gemini-code-project", location="us-central1")
     model = GenerativeModel(
-        "gemini-1.5-pro",
+        "gemini-1.5-flash",
         system_instruction=[prompt]
     )
     new_message = f"### {message} ###"
@@ -35,5 +36,10 @@ def generate(message, prompt=get_instructions_prompt()):
         safety_settings=safety_settings,
         stream=True,
     )
-    return responses[0].text
+    final_string = ""
+    for response in responses:
+        final_string += response.text
+        print(response.text, end="")
+
+    return final_string
 
